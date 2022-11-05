08debd635edd154dc075e32ce952babbdc910a8d
Sarah Choate <87721944+sechoate@users.noreply.github.com>
2022-11-05 12:36:53 -0500

    Update singleEvent.c

--- full-simulation-analysis/singleEvent.c
+++ full-simulation-analysis/singleEvent.c
@@ -1,7 +1,11 @@
+/*
+This code outputs all properties for a single event using the genie level simulation file 
+ */
+
 void singleEvent(){
-  TFile* f = new TFile("prodgenie_nutau_dune10kt_gen.root");
+  TFile* f = new TFile("prodgenie_nutau_dune10kt_gen.root");    //name of genie level file 
   //f->ls();
   TTree* t = (TTree *) f->Get("Events");
-  t->Show(30);
+  t->Show(30);     //event number, iterates from 0 
 }
 
